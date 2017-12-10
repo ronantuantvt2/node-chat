90a4ec17b3c26770eab9cff8ebc28f94e2a6fc47
ronantuantvt@gmail.com <ronantuantvt@gmail.com>
2017-12-11 01:42:27 +0700

    Connect, disconnect event

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 const path      = require('path');
+const http      = require('http');
 const express   = require('express');
+const socketIO  = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.port || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New connection');
+    socket.on('disconnect', () => {
+       console.log('Disconnect to browser') ;
+    });
+});
+
+server.listen(port, () => {
    console.log(`Server is running or port ${port}`);
 });
 
