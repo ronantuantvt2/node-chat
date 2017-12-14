7ba57be3f21dd89dce0a9aa04a2534375822aeb5
ronantuantvt@gmail.com <ronantuantvt@gmail.com>
2017-12-14 18:17:58 +0700

    add acknowledge

--- public/js/index.js
+++ public/js/index.js
@@ -9,3 +9,10 @@ socket.on('disconnect', () => {
 socket.on('newMessage', (message) => {
     console.log(message);    
 });
+
+socket.emit('createMessage', {
+  from: 'Ronan',
+  text: 'Hello'
+}, function(data) {
+  console.log(data);
+});
